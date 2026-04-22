7d5f6ad41bb03a47a378bdb29ca378a2cb729f03
nessab35 <nessa_begay.35@icloud.com>
2026-04-21 20:53:42 -0500

    changed amount of bots at each task for better win consistency

--- main.mjs
+++ main.mjs
@@ -1,8 +1,72 @@
-import { } from 'game/utils';
-import { } from 'game/prototypes';
-import { } from 'game/constants';
-import { } from 'arena/season_beta/capture_the_flag/basic';
+import { getObjectsByPrototype } from "game/utils";
+import {
+	Creep,
+	Flag,
+	StructureContainer,
+	StructureTower,
+} from "game/prototypes";
+import { RESOURCE_ENERGY, ERR_NOT_IN_RANGE } from "game/constants";
+
+function runDefenders(defenders, enemies, container, tower, myFlag) {
+	// defender logic
+	for (var i = 0; i < defenders.length; i++) {
+		var creep = defenders[i];
+		var target = creep.findInRange(enemies, 10)[0];
+		if (target) {
+			// enemy spotted- shoot them
+			creep.rangedAttack(target);
+		} else if (i == 0) {
+			// first defender feeds the tower
+			if (creep.store[RESOURCE_ENERGY] == 0) {
+				if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(container);
+				}
+			} else {
+				if (creep.transfer(tower, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(tower);
+				}
+			}
+		} else {
+			// rest of defenders guard flag
+			creep.moveTo(myFlag);
+		}
+	}
+}
+
+function runRunners(runners, enemies, enemyFlag) {
+	// runner logics
+	for (var creep of runners) {
+		var target = creep.findInRange(enemies, 10)[0];
+		if (target) {
+			// enemy spotted- shoot them
+			creep.rangedAttack(target);
+		}
+		// prioritize going to enemy flag
+		creep.moveTo(enemyFlag);
+	}
+}
+
+function runFighters(fighters, enemies) {
+	// fighter logic
+	for (var creep of fighters) {
+		var target = creep.findClosestByRange(enemies);
+		// fighting only
+		if (target) {
+			creep.rangedAttack(target);
+			creep.moveTo(target);
+		}
+	}
+}
 
 export function loop() {
-    // Your code goes here
+	var enemyFlag = getObjectsByPrototype(Flag).find((object) => !object.my);
+	var myFlag = getObjectsByPrototype(Flag).find((object) => object.my);
+	var myCreeps = getObjectsByPrototype(Creep).filter((object) => object.my);
+	var enemies = getObjectsByPrototype(Creep).filter((object) => !object.my);
+	var tower = getObjectsByPrototype(StructureTower).find((object) => object.my);
+	var container = getObjectsByPrototype(StructureContainer)[0];
+
+	runDefenders(myCreeps.slice(0, 2), enemies, container, tower, myFlag);
+	runRunners(myCreeps.slice(2, 9), enemies, enemyFlag);
+	runFighters(myCreeps.slice(9), enemies);
 }
